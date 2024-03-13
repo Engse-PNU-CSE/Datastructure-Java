147fc846a584d02b514a6c6ba2e63462168bd90e
Kim Engse <error302@naver.com>
2024-03-13 09:18:45 +0900

    24.03.13 v1

--- ch03/TextCode.java
+++ ch03/TextCode.java
@@ -1,11 +1,23 @@
 package datastructure.ch03;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class TextCode {
 	public static void main(String[] args) {
-		String str = "     a";
-		System.out.println(str.trim());
+		String[] str = {"apple", "banana", "Char", "apple"};
+		List<String> list = new ArrayList<>(Arrays.asList(str));
+		List<String> list2 = new ArrayList<>();
+		Iterator<String> iter = list.iterator();
+		while(iter.hasNext()) {
+			String s = iter.next();
+			if(list2.contains(s)) continue;
+			else list2.add(s);
+		}
+		System.out.println(list2);
 	}
 	
 }
